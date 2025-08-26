b8f2dbb8f76cd248f5ed77003c1f7739fe24e9c3
aida wu <ada10050616@gmail.com>
2025-08-26 16:36:42 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Navbar from './components/global/Navbar.vue'
+</script>
+
+<template>
+  <Navbar />
+</template>
---
+++ src/components/global/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="navbar">
+        <div class="logo">
+            <div class="aida">
+                <a href="">ɐida</a>
+            </div>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="">Project</a></li>
+                <li><a href="">Resources</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+@use "@/assets/css/components/variables" as *;
+
+.navbar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 8rem;
+    background-color: $color-bg;
+    .menu {
+        ul{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    }
+}
+</style>
